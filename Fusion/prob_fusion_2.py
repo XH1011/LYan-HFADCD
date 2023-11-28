cc001381423dddcb3a72c2ca0161689ee30ff390
XH1011 <127925263+XH1011@users.noreply.github.com>
2023-11-28 14:56:43 +0800

    Add files via upload

---
+++ Fusion/prob_fusion_2.py
@@ -0,0 +1,78 @@
+import pickle
+import numpy as np
+import pandas as pd
+
+labels = np.squeeze(np.concatenate((np.zeros((300, 1), dtype=int), np.full((1800, 1), -1, dtype=int))))
+df_result = pd.DataFrame()
+for j in range(1, 11):
+    print('实验次数:',j)
+    # iforest
+    # # #original
+    # df = pd.read_excel('D:/Desktop/paper1/experiment/Prob_iforest_original_4metrics.xlsx', sheet_name='Sheet' + str(j))
+    #encoded
+    df = pd.read_excel('D:/Desktop/paper1/experiment/Prob_iforest_4metrics.xlsx', sheet_name='Sheet' + str(j))
+    prob_n_ifo = df.iloc[:, 1].values
+    prob_ab_ifo = df.iloc[:, 2] .values
+    accuracy_ifo = df['Accuracy'].iloc[0]
+    print('Anomaly detection：', accuracy_ifo)
+
+    #clustering
+    # #original
+    # df = pd.read_excel('D:/Desktop/paper1/DSClustering/Results_original/Result'+str(j)+'/reg_1_1_1.xlsx', sheet_name='Sheet1')
+    #encoded
+    df = pd.read_excel('D:/Desktop/paper1/DSClustering/Results/Result'+str(j)+'/reg_1_1_1.xlsx', sheet_name='Sheet1')
+
+    prob_C0_cls = df.iloc[:, 4].values # 如果 prob 是第一列
+    prob_C1_cls = df.iloc[:, 5].values
+    prob_C2_cls = df.iloc[:, 6].values
+    prob_C3_cls = df.iloc[:, 7].values
+    prob_C4_cls = df.iloc[:, 8].values
+    prob_C5_cls = df.iloc[:, 9].values
+    prob_C6_cls = df.iloc[:, 10].values
+    accuracy_cls = round(df['acc_x'].iloc[0], 4)
+    print('Fault clustering', accuracy_cls)
+
+    for k in range(11):
+        print(k)
+        w_ifo = k/10
+        w_cls = 1-w_ifo
+        prob_n = w_ifo*prob_n_ifo +w_cls*prob_C0_cls
+        prob_f = 1-prob_n
+        pre_labels = np.squeeze(np.zeros(labels.shape, dtype=int))
+        for i in range(len(labels)):
+            pre_labels[i] = {0: 0, 1: -1}[np.argmax([prob_n[i],prob_f[i]])]
+        n_error=0
+        for i in range(len(labels)):
+            if labels[i] != pre_labels[i]:
+                n_error+=1
+        accuracy = 1- (n_error/len(labels))
+        accuracy = round(accuracy,4)
+        print("fusion accuracy", accuracy)
+        TP = np.sum((labels == 0) & (pre_labels == 0))
+        TN = np.sum((labels == -1) & (pre_labels == -1))
+        FP = np.sum((labels == -1) & (pre_labels == 0))
+        FN = np.sum((labels == 0) & (pre_labels == -1))
+        precision = TP / (TP + FP)
+        # print('precision', precision)
+        recall = TP / (TP + FN)
+        # print('recall', recall)
+        F1_score = 2 * (precision * recall) / (precision + recall)
+        print('fusio F1_score', F1_score)
+
+
+        # # 将结果DataFrame写入Excel文件
+        # data = {f'{k}': [k]}
+        # id_df = pd.DataFrame(data)
+        # accuracy_df = pd.DataFrame({'Accuracy': [accuracy]})
+        # F1_df = pd.DataFrame({'F1_score': [F1_score]})
+        # df_result = pd.concat([df_result,  id_df,accuracy_df,F1_df], axis=1)
+        # with pd.ExcelWriter('D:/Desktop/paper1/experiment/fusion_2-class2.xlsx', engine='openpyxl', mode='w') as writer:
+        #     df_result.to_excel(writer, sheet_name='Sheet1',  float_format='%.4f')
+# 将结果DataFrame写入Excel文件
+        data = {f'{k}': [F1_score]}
+        id_df = pd.DataFrame(data)
+        # accuracy_df = pd.DataFrame({'Accuracy': [accuracy]})
+        # F1_df = pd.DataFrame({'F1_score': [F1_score]})
+        df_result = pd.concat([df_result,  id_df], axis=1)
+        with pd.ExcelWriter('D:/Desktop/paper1/experiment/fusion_2-class2.xlsx', engine='openpyxl', mode='w') as writer:
+            df_result.to_excel(writer, sheet_name='Sheet1',  float_format='%.4f')
